943cfea573ed3283abfc05fce2b3b85863dbf2a2
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-19 09:12:21 +0530

    git remote add origin https://github.com/dac-2022/day12.git

---
+++ 1.js
@@ -0,0 +1,18 @@
+const makeAjaxCall = async () => {
+  const url = "https://reqres.in/api/users";
+
+  const res = await axios.get(url);
+
+  for (let item of res.data.data) {
+    const output = document.querySelector("#output");
+
+    const newElement = `<div style="background: lightblue; display: flex; margin-bottom: 8px">
+                            <img src="${item.avatar}" alt="" />
+                            <div>
+                            <h3>${item.first_name} ${item.last_name}</h3>
+                            <h6>${item.email}</h6>
+                            </div>
+                        </div>`;
+    output.innerHTML = newElement + output.innerHTML;
+  }
+};
